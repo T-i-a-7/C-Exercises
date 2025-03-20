96c802d8b6e740637eff9283be3d7c859238967a
T-i-a-7 <155762535+T-i-a-7@users.noreply.github.com>
2025-03-20 19:54:45 +0100

    Update Exercise-2.c

--- 2025-02-03/Exercise-2.c
+++ 2025-02-03/Exercise-2.c
@@ -7,11 +7,25 @@ perimetro (4 fn)
 
 #include <stdio.h>
 
+void acquisizione(float a, float b, float c) {
+    
+}
 
+void controllo(float a, float b, float c) {
+
+}
+
+float calcolo(float a, float b, float c) {
+
+}
+
+void visualizzazione(float P) {
+    printf("Perimetro: %f", &P);
+}
 
 int main() {
 
-    float a, b, c;
+    float a, b, c, p;
 
     int i;
     
