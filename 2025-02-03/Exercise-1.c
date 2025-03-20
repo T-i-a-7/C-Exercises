5f2046fd8b3fa85eed6af0d46148ce462dde23c5
T-i-a-7 <mattia-7r@proton.me>
2025-03-20 18:57:49 +0100

    Update Exercise-1.c

--- 2025-02-03/Exercise-1.c
+++ 2025-02-03/Exercise-1.c
@@ -1,12 +1,43 @@
 /*
-Acquisito un valore intero positivo, visualizzare un messaggio che indica se è primo oppure no (2 fn)
+Acquisito un valore intero positivo, visualizzare un messaggio che indica se è primo oppure no (2 funzioni)
 */
 
 
 
 #include <stdio.h>
 
-int main () {
+int primo(int X) {
+    int i;
+
+    if (X<=1) {
+        return 0;
+    }
+    for (i=2; i*i<=X; i++) {
+        if (X % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void messaggio(int X) {
+    if(primo(X)) {
+        printf("Il numero %d è primo.\n", X);
+    } else {
+        printf("Il numero %d non è primo.\n", X);
+    }
+}
+
+int main() {
+
+    int x;
+
+    do {
+        printf("Fornisci valore: ");
+        scanf("%d", &x);
+    } while (x<=0);
+
+    messaggio(x);
 
     return 0;
 }
