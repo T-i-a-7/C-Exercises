38afd4ede7d75f12abc3301be81ab43b36734d17
T-i-a-7 <mattia-7r@proton.me>
2025-03-20 19:12:35 +0100

    Update Exercise-2.c

--- 2025-02-03/Exercise-2.c
+++ 2025-02-03/Exercise-2.c
@@ -2,3 +2,24 @@
 Acquisiti 3 valori, controllare che siano i lati di un triangolo ed in quel caso calcolare e visualizzare il
 perimetro (4 fn) 
 */
+
+
+
+#include <stdio.h>
+
+
+
+int main() {
+
+    float a, b, c;
+
+    int i;
+    
+    for (i=0; i<3; i++) {
+        do {
+            printf("Fornisci tre valori: %f %f %f");
+        } while ()
+    }
+
+    return 0;
+}
