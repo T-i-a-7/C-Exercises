92b04f071e3b584ff70fdafc626066ef409dbd11
T-i-a-7 <mattia-7r@proton.me>
2025-05-22 18:37:37 +0200

    Create files

---
+++ 2025-05-09/Exercise-1.c
@@ -0,0 +1,46 @@
+Per una riunione del G20 vengono accreditati gli assaggiatori associati agli ospiti governativi. Per ciascun assaggiatore si devono gestire le seguenti informazioni:
+
+ 
+
+Codice (max 7 caratteri)
+
+Delegazione (codice che identifica la nazionalità della delegazione: max 5 caratteri)
+
+Codice del delegato a cui è associato (max 7 caratteri)
+
+Data ultimo assaggio
+
+Numero di assaggi effettuati
+
+Ricovero (S/N)
+
+ 
+
+Si deve prevedere un menù ripetibile con le seguenti opzioni:
+
+ 
+
+    Inserimento di un nuovo assaggiatore                                                                    
+    Cancellazione di un assaggiatore                                                                            
+    Gestione assaggio                                                                            
+    Assaggiatori per delegazione                                                                                  
+    Statistiche                                                                                                    
+    Fine
+
+ 
+
+La prima richiesta prevede di inserire i dati di un nuovo assaggiatore, chiedendo all'utente le sole informazioni necessarie.
+
+Il secondo punto prevede la cancellazione di un assaggiatore perché non più disponibile. Si riceve il codice dell’assaggiatore e si devono visualizzare il codice della delegazione di appartenenza ed il numero di assaggi eseguiti.
+
+Il terzo punto prevede di gestire l’aggiornamento del numero di assaggi eseguiti incrementando di uno il campo ed inserendo la nuova data.
+
+La quarta richiesta prevede di costruire un vettore che contenga i codici ed il numero di assaggi effettuati dagli assaggiatori di una delegazione di cui viene fornito il codice; si deve poi visualizzare tale vettore e visualizzare quanti ricoveri sono stati effettuati nella delegazione.
+
+La quinta richiesta prevede di calcolare la media degli assaggi effettuati rispetto agli assaggiatori e la percentuale di ricoveri effettuati rispetto al numero di assaggi effettuati; si deve inoltre visualizzare il numero di assaggiatori associati ad un ospite di cui viene dato il codice.
+
+Si preveda che le informazioni siano già presenti in memoria e che sia stata impostata la variabile N con il numero di assaggiatori fino ad oggi inseriti. In assenza di specifiche si effettuino le ipotesi necessarie.
+
+Si richiede l’impostazione di un programma in C scritto in base alle regole della programmazione strutturata, utilizzando, dove possibile, per la ricerca il metodo dicotomico e per l'ordinamento l'algoritmo bubblesort. 
+
+Si impostino almeno 3 con il return e 3 con il passaggio parametri per indirizzo. Si devono escludere i vettori nel conteggio del passaggio parametri per indirizzo.
---
+++ 2025-05-17/Exercise-1.c
@@ -0,0 +1,39 @@
+/*
+Per un amministratore condominiale si deve impostare un programma che effettui alcune elaborazioni sui condomini che amministra.
+Per ciascun condominio si devono gestire le seguenti informazioni:
+    Codice (max 5 caratteri)
+    Numero appartamenti
+    Ascensore (S/N)
+    Anno inizio mandato
+    Numero di lavori straordinari nell’anno corrente
+
+Si deve prevedere un menù ripetibile con le seguenti opzioni:
+    1. Inserimento di un nuovo condominio                                                                     
+    2. Cancellazione di un condominio                                                                            
+    3. Modifiche                                                                            
+    4. Lista condomini con lavori nell’anno corrente                                                                   
+    5. Statistiche                                                                                                   
+    6. Fine
+
+La prima richiesta prevede di inserire i dati di un nuovo condominio, chiedendo all'utente le sole informazioni necessarie. Si
+deve ordinare in base al codice. Il secondo punto prevede la cancellazione di un condominio perché passato ad un altro amministratore.
+Si riceve il codice del condominio e si deve visualizzare il numero di anni di mandato chiedendo all’utente l’anno corrente. Il terzo
+punto prevede di gestire la modifica della presenza di ascensore (sostituzione) e/o il numero di lavori straordinari nell’anno corrente
+(incremento). Si riceve il codice del condominio. La quarta richiesta prevede di costruire un vettore che contenga i codici ed il
+numero di lavori straordinari nell’anno corrente per quei condomini che hanno lavori straordinari; si deve poi visualizzare tale lista.
+La quinta richiesta prevede di calcolare la percentuale dei condomini con ascensore e la media del numero di appartamenti considerando
+tutti i condomini; si deve inoltre visualizzare il codice del condominio di cui l’amministratore ha mandato più lungo, chiedendo l’anno
+corrente all’utente. Si preveda che le informazioni siano già presenti in memoria e che sia stata impostata la variabile N con il numero
+di condomini fino ad oggi inseriti. In assenza di specifiche si effettuino le ipotesi necessarie. Per l'ordinamento si utilizzi
+l'algoritmo bubblesort. Si impostino almeno 3 funzioni con il return e 3 con il passaggio parametri per indirizzo. Si devono escludere i
+vettori nel conteggio del passaggio parametri per indirizzo.
+*/
+
+
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
